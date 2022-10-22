1a512014b209754509226d196006c0c3987d02e6
Omraj <omraj7413@gmail.com>
2022-10-22 20:00:13 +0530

    added products

--- src/component/products/ProductCard.jsx
+++ src/component/products/ProductCard.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 import './ProductCard.css'
 import {useDispatch} from 'react-redux'
 
-function ProductCard() {
+function ProductCard(
+  {
+    id,
+    name,
+    description,
+    price,
+    image
+  }
+) {
 
   const dispatch = useDispatch()
 
   return (
     <div className='card-container'>
         <div className='card-image'>
-            <img src='https://m.media-amazon.com/images/I/71ey-9D8yDL._AC_UY436_FMwebp_QL65_.jpg' />
+            <img src={image} />
         </div>
         <div className='card-content'>
-            <h3 className='product-name'>Product Name</h3>
-            <p className='product-desc'>27.69 cm (10.9-inch) Liquid Retina display1 with True Tone, P3 wide colour and an anti-reflective coating</p>
-            <p className='product-price'> $ 51,999.00</p>
+            <h3 className='product-name'>{name}</h3>
+            <p className='product-desc'>{description}</p>
+            <p className='product-price'> $ {price}</p>
         </div>
         <div className='card-button-container'>
             <button 
--- src/component/products/SmartProduct.jsx
+++ src/component/products/SmartProduct.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import './SmartProduct.css'
+
+const ProductList = [
+  {
+    id: 1,
+    name: 'Apple Ipad Pro',
+    description: '27.69 cm (10.9-inch) Liquid Retina display1 with True Tone, P3 wide colour and an anti-reflective coating',
+    price: 51999,
+    image: 'https://m.media-amazon.com/images/I/71ey-9D8yDL._AC_UY436_FMwebp_QL65_.jpg'
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 12 (64GB) - Green',
+    description: '6.1-inch (15.5 cm diagonal) Super Retina XDR display',
+    price: 69900,
+    image: 'https://m.media-amazon.com/images/I/71cQWYVtcBL._AC_UY436_FMwebp_QL65_.jpg'
+  },
+  {
+    id: 3,
+    name: 'Apple Watch SE',
+    description: 'GPS + Cellular, 40mm',
+    price: 29900,
+    image: 'https://m.media-amazon.com/images/I/71ZyBh4LQuL._AC_UY436_FMwebp_QL65_.jpg'
+  },
+  {
+    id: 4,
+    name: 'Apple AirPods Pro',
+    description: 'Active Noise Cancellation for immersive sound',
+    price: 23900,
+    image: 'https://m.media-amazon.com/images/I/61SUj2aKoEL._AC_UY436_FMwebp_QL65_.jpg'
+  }
+]
 
 function SmartProduct() {
   return (
-    <div>SmartProduct
-        <ProductCard/>
+    <div className='smart-product-container'>
+      <div className='smart-product-header'>
+        SmartProduct
+      </div>
+
+      <div className='smart-product-list'>
+        {
+          ProductList.map((product) => {
+            return (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                image={product.image}
+              />
+            )
+          })
+        }
+      </div>
+
     </div>
   )
 }
